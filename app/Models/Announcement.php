d1df0000ac164a900c9ce15a34b9239df47eb9c1
foulegold@gmail.com <foulegold@gmail.com>
2021-10-24 15:08:37 +0700

    Поиск объявлений по фильтрам

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use mysqli;
 
 /**
  * @property User user
@@ -78,8 +79,54 @@ class Announcement extends Model
         return $result;
     }
 
+    // Вероятные фильтры:
+    // category_id, date_from, date_to, price_from, price_to, title
     public static function getAllAnnouncements(Request $request)
     {
-        dd($request);
+        $params = $request->request->all();
+
+        // Обработаем параметры от sql-инъекций
+        // TODO: добавить более детальную обработку параметров
+        foreach ($params as $key => &$param) {
+            switch ($key) {
+                case 'category_id':
+                    $param = intval($param);
+                    break;
+                case 'date_from':
+                case 'date_to':
+                    $param = date('Y-m-d', strtotime($param));
+                    break;
+                case 'price_from':
+                case 'price_to':
+                    $param = floatval($param);
+                    break;
+                case 'title':
+                    $param = trim($param);
+                    $param = htmlspecialchars($param);
+                    $param = addslashes($param);
+                    break;
+            }
+        }
+
+        $whereParams = [
+            ['status', '=', 'active'],
+        ];
+        if (isset($params['category_id']) and $params['category_id'] != '') {
+            $whereParams[] = ['category_id', '=', $params['category_id']];
+        }
+        if (isset($params['price_from']) and $params['price_from'] != '') {
+            $whereParams[] = ['price', '>=', $params['price_from']];
+        }
+        if (isset($params['price_to']) and $params['price_to'] != '' and $params['price_to'] > 0) {
+            $whereParams[] = ['price', '<=', $params['price_to']];
+        }
+        if (isset($params['title']) and $params['title'] != '') {
+            $whereParams[] = ['title', 'like', "%{$params['title']}%"];
+        }
+
+        $result = Announcement::where($whereParams)->order('created_at', 'ASC')->get()->toArray();
+
+        // TODO: Добавить фильтр по свободной дате
+        return $result;
     }
 }
