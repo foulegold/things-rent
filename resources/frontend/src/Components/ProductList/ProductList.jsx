2436ab7b0e366cf786f9205998e6e43efbec7edc
nikita <nikitavrnk@gmail.com>
2021-10-28 11:10:50 +0300

    исправлены мелкие недочеты

--- resources/frontend/src/Components/ProductList/ProductList.jsx
+++ resources/frontend/src/Components/ProductList/ProductList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import s from "./ProductList.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-// import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const obj = {
     product: [
