e9a4bfda4b281c4848399cedad1ede0ff9167ddc
Feniksdv <feniksdv@gmail.com>
2021-10-29 15:13:56 +0300

    Актуализировал ReDME, Создал два Рвеста для проверки входящих данных, Написал логику обработки Объявлений. Теперь их можно по АПИ создать новую, удалить, обновить и вывести все. Пока без погинации надо продумать пагинацию для всего проекта и составить один конфиг фаил.

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Api\AnnouncementController as ApiAnnouncementController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//announcement — CRUD (кому-то надо переделать маршрут... или оставить как есть)
+Route::resource('announcement', ApiAnnouncementController::class)->middleware(['api', 'auth:web']);
 
 Route::group(['prefix' => 'announcement'], function () {
     Route::match(['get', 'post'], '/get-all', [AnnouncementController::class, 'getAllAnnouncements']);
