2443b696ae435fad1e1f307529bd50af85211edc
NikitaVrnk <62518608+NikitaVrnk@users.noreply.github.com>
2021-10-31 16:18:28 +0300

    верстка футера (#58)

--- resources/frontend/src/Components/Router/Router.js
+++ resources/frontend/src/Components/Router/Router.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ProductList from "../ProductList/ProductList";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
 
 
 function Router() {
@@ -10,8 +12,9 @@ function Router() {
         <Route exact path="/header" component={Header}></Route>
         {/* ваши роуты или приватРоуты*/}
         {/* ваши роуты или приватРоуты*/}
-        {/* ваши роуты или приватРоуты*/}
         <Route exact path="/ProductList" component={ProductList}></Route>
+        <Route exact path="/Footer" component={Footer}></Route>
+
       </Switch>
     </BrowserRouter>
   );
