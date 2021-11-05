98b357c6f1d9eaa9a5e3b7193d5f0317abe84581
foulegold@gmail.com <foulegold@gmail.com>
2021-11-05 09:31:56 +0700

    added new field in categories table; remade by lecturer commentaries

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\Api\AnnouncementController as ApiAnnouncementController;
+use App\Http\Controllers\Api\AnnouncementController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 //announcement — CRUD (кому-то надо переделать маршрут... или оставить как есть)
-Route::resource('announcement', ApiAnnouncementController::class)->middleware(['api', 'auth:web']);
+Route::resource('announcement', AnnouncementController::class)->middleware(['api', 'auth:web']);
 
 Route::group(['prefix' => 'announcement'], function () {
     Route::get('/all', [AnnouncementController::class, 'all']);
     Route::get('/{announcement}', [AnnouncementController::class, 'one']);
-    Route::get('/{announcement}/calendar', [AnnouncementController::class, 'calendar']);
+//    Route::get('/{announcement}/calendar', [AnnouncementController::class, 'calendar']);
 });
 
 Route::group(['prefix' => 'category'], function () {
