3bc9dd11b09341b702ecb76cee90454f48481dd6
Feniksdv <feniksdv@gmail.com>
2021-11-05 08:42:04 +0300

    Поправил Роуты. Поправил Сидеры.

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//announcement — CRUD (кому-то надо переделать маршрут... или оставить как есть)
-Route::resource('announcement', AnnouncementController::class)->middleware(['api', 'auth:web']);
-
 Route::group(['prefix' => 'announcement'], function () {
     Route::get('/all', [AnnouncementController::class, 'all']);
     Route::get('/{announcement}', [AnnouncementController::class, 'one']);
 //    Route::get('/{announcement}/calendar', [AnnouncementController::class, 'calendar']);
 });
+
+//announcement — CRUD (кому-то надо переделать маршрут... или оставить как есть)
+Route::resource('announcement', AnnouncementController::class)->middleware(['api', 'auth:web']);
 
 Route::group(['prefix' => 'category'], function () {
     Route::get('/all', [CategoryController::class, 'all']);
