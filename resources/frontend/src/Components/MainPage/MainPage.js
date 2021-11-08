4cec466dd48eadb27fb0d08c0cf0d48ddc6fba91
Петр Хомченко <70071692+K-y-o-n@users.noreply.github.com>
2021-11-08 05:14:12 +0300

    запрос на получение категорий с сервера (#66)

--- resources/frontend/src/Components/MainPage/MainPage.js
+++ resources/frontend/src/Components/MainPage/MainPage.js
@@ -1,8 +1,20 @@
 import Header from "../Header/Header";
 import ProductList from "../ProductList/ProductList";
 import Footer from "../Footer/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getAllCategories } from "../../store/actions/categoriesAction";
+
 
 function MainPage() {
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(getAllCategories())
+  }, [])
+
+
   return (
     <>
       <Header />
