f782ea0566d63def5febc99a668a122a4ced0c1c
Петр <s.leonhart@mail.ru>
2021-11-10 23:11:53 +0300

    верстка страницы объявления

--- resources/frontend/src/Components/Router/Router.js
+++ resources/frontend/src/Components/Router/Router.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AnnouncementMiniCard from "../AnnouncementMiniCard/AnnouncementMiniCard";
+import AnnouncementPage from "../AnnouncementPage/AnnouncementPage";
 import FilteredAnnouncements from "../FilteredAnnouncements/FilteredAnnouncements";
 import MainPage from "../MainPage/MainPage";
+
 
 function Router() {
   return (
@@ -9,11 +10,11 @@ function Router() {
       <Switch>
         <Route exact path="/" component={MainPage}></Route>
         <Route exact path="/filter" component={FilteredAnnouncements}></Route>
-        <Route exact path="/card" component={AnnouncementMiniCard}></Route>
-        {/* ваши роуты или приватРоуты*/}
-        {/* ваши роуты или приватРоуты*/}
-
-
+        <Route
+          exact
+          path="/announcement/:id"
+          component={AnnouncementPage}
+        ></Route>
       </Switch>
     </BrowserRouter>
   );
